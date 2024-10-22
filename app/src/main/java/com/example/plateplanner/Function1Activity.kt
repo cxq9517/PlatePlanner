fc208e1c0e26b8f810d00fb9b418e2262b25845d
cxq9517 <cxq9517@mavs.uta.edu>
2024-10-22 15:40:53 -0500

    api integration

--- app/src/main/java/com/example/plateplanner/Function1Activity.kt
+++ app/src/main/java/com/example/plateplanner/Function1Activity.kt
@@ -1,16 +1,45 @@
 package com.example.plateplanner
 
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class Function1Activity : AppCompatActivity() {
+
+    private val repository = RecipeRepository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_function1)
 
+        // Test ingredients and API key
+        val testIngredients = "tomato, cheese"
+        val apiKey = "ea087b3041814c9d874cb0a459ed7cf1"
 
+        // Call the repository method
+        repository.getRecipesByIngredients(testIngredients, apiKey) { recipes ->
+            if (recipes != null) {
+                // Log the first recipe title
+                Log.d("TEST_RESULT", "First Recipe: ${recipes[0].title}")
+            } else {
+                Log.e("TEST_RESULT", "Failed to get recipes")
+            }
+        }
     }
+
+
 }
--- app/src/main/java/com/example/plateplanner/Recipe.kt
+++
@@ -1,6 +0,0 @@
-package com.example.plateplanner
-
-data class Recipe(
-    val name: String,
-    val imageUrl: String
-)
